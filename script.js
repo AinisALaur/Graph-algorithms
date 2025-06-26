28824ad565ceed511875e85de99d30e09b8bde90
Ainis Laurinavičius <ainislaurinavicius@gmail.com>
2025-06-26 18:07:23 +0300

    Added function to connect nodes

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const nodes = [];
+let highlightedNodes = new Set();
+
 function getMousePos(event) {
     let x = event.clientX;
     let y = event.clientY;
@@ -23,8 +25,19 @@ function isOverlaping(x1, y1){
     return false;
 }
 
+function getNodeId(x1, y1){
+    for(let i = 0;  i < nodes.length; ++i){
+        let x2 = nodes[i][0];
+        let y2 = nodes[i][1]; 
+
+        if(getDistance(x1,y1,x2,y2) < 40){
+            return i;
+        }
+    }
+}
+
 function isInBounds(x, y){
-    var offsets = document.getElementById('Nodes').getBoundingClientRect();
+    var offsets = document.getElementById("Canvas").getBoundingClientRect();
     var top = offsets.top;
     var left = offsets.left;
 
@@ -34,29 +47,90 @@ function isInBounds(x, y){
     return false;
 }
 
-function addToArray(event){
+function drawNewNode(x, y){
+    const newNode = document.createElement("div");
+    const newContent = document.createTextNode((nodes.length + 1).toString());
+    newNode.appendChild(newContent);
+
+    newNode.classList.add('Node');
+    newNode.id = (nodes.length).toString();
+
+    newNode.style.position = "absolute";
+    newNode.style.left = x + 'px';
+    newNode.style.top = y + 'px';
+
+    console.log("Node drawn ", x, y);
+
+    const parent = document.getElementById("Canvas");
+    parent.appendChild(newNode);
+}
+
+function deHighlightNode(id){
+    const node = document.getElementById(id);
+    node.style.border = "none";
+    highlightedNodes.delete(id);
+}
+
+function highlightNode(x, y){
+    let id = getNodeId(x, y);
+    if(highlightedNodes.has(id) == false){
+        const node = document.getElementById(id);
+        node.style.border = "thick solid #f5e942";
+        highlightedNodes.add(id);
+    }else{
+        deHighlightNode(id);
+    }
+}
+
+function connectNodes(){
+    const coordinates = [];
+
+    const canvasContainer = document.getElementById("Canvas");
+    const containerRect = canvasContainer.getBoundingClientRect();
+
+    for(const id of highlightedNodes){
+        let x = nodes[id][0] - containerRect.left + 25;
+        let y = nodes[id][1] - containerRect.top + 40;
+        coordinates.push([x,y]);
+        deHighlightNode(id);
+        highlightedNodes.delete(id);
+    }
+
+    const c = document.getElementById("myCanvas");
+    const ctx = c.getContext("2d");
+
+    X1 = coordinates[0][0];
+    Y1 = coordinates[0][1];
+
+    X2 = coordinates[1][0];
+    Y2 = coordinates[1][1];
+
+    console.log(X1, Y1, X2, Y2);   
+    
+    ctx.beginPath();
+    ctx.moveTo(X1, Y1);
+    ctx.lineTo(X2, Y2);
+    ctx.strokeStyle = 'black';
+    ctx.lineWidth = 3;
+    ctx.stroke();
+}
+
+function getCoords(event){
     const coords = getMousePos(event);
-    let x = coords[0] - 20;
-    let y = coords[1] - 50;
+    let x = coords[0] - 25;
+    let y = coords[1] - 25;
 
     if(isOverlaping(x,y) == false && isInBounds(x, y)){
         drawNewNode(x,y);
         nodes.push([x,y]);
     }
+    
+    else{
+        highlightNode(x,y);
+        if(highlightedNodes.size == 2){
+            connectNodes();
+        }
+    }
 }
 
-function drawNewNode(x, y){
-  const newNode = document.createElement("div");
-  const newContent = document.createTextNode((nodes.length + 1).toString());
-  newNode.appendChild(newContent);
-
-  newNode.classList.add('Node');
-  newNode.style.position = "absolute";
-  newNode.style.left = x + 'px';
-  newNode.style.top = y + 'px';
-
-  const parent = document.getElementById("Nodes");
-  parent.appendChild(newNode);
-}
-
-document.addEventListener("click", addToArray);
+document.addEventListener("click", getCoords);
